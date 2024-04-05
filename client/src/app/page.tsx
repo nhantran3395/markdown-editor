030b6dc5641b7e65df8081c91ac1987da66236f3
nhantran3395 <thanhnhan33.95@gmail.com>
2024-04-05 15:23:22 +0700

    feat: implement toggle between normal/full preview

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,20 +1,40 @@
+"use client";
+import { useState } from "react";
+import { clsx } from "clsx";
+
 import { SubHeader } from "@/components/editor/sub-header";
 import { MarkdownPane } from "@/components/editor/markdown-pane";
 import { PreviewPane } from "@/components/editor/preview-pane";
 
 export default function Home() {
+  const [fullPreview, setFullPreview] = useState(false);
+
+  function onToggleFullPreview() {
+    setFullPreview((prev) => !prev);
+  }
+
   return (
     <>
-      <SubHeader />
+      <SubHeader
+        fullPreview={fullPreview}
+        onToggleFullPreview={onToggleFullPreview}
+      />
       <section className={"flex flex-1 overflow-auto"}>
+        {fullPreview ? null : (
+          <div
+            className={
+              "flex-1 py-4 pl-4 border-r-gray-200 border-r-[1px] overflow-auto"
+            }
+          >
+            <MarkdownPane />
+          </div>
+        )}
         <div
-          className={
-            "flex-1 py-4 pl-4 border-r-gray-200 border-r-[1px] overflow-auto"
-          }
+          className={clsx(
+            "flex-1 py-4 pl-4 overflow-auto",
+            !fullPreview && "max-sm:hidden",
+          )}
         >
-          <MarkdownPane />
-        </div>
-        <div className={"flex-1 py-4 pl-4 max-sm:hidden overflow-auto"}>
           <PreviewPane />
         </div>
       </section>
--- client/src/components/editor/sub-header.tsx
+++ client/src/components/editor/sub-header.tsx
@@ -1,16 +1,50 @@
-import PreviewIcon from "@/assets/icons/icon-show-preview.svg";
+import { clsx } from "clsx";
 
-export function SubHeader() {
+import ShowPreviewIcon from "@/assets/icons/icon-show-preview.svg";
+import HidePreviewIcon from "@/assets/icons/icon-hide-preview.svg";
+
+type SubHeaderProps = {
+  fullPreview: boolean;
+  onToggleFullPreview: () => void;
+};
+
+export function SubHeader({
+  fullPreview,
+  onToggleFullPreview,
+}: Readonly<SubHeaderProps>) {
   return (
     <header className="w-full bg-gray-100 flex relative">
-      <div className={"flex-1 py-3 pl-4"}>
-        <h2 className={"text-gray-400 text-heading-s"}>MARKDOWN</h2>
-      </div>
-      <div className={"w-[1px] h-[full] border-r-gray-200 border-r-[1px]"} />
-      <div className={"flex-1 py-3 pl-4 max-sm:hidden"}>
+      {fullPreview ? null : (
+        <>
+          <div
+            className={clsx("flex-1 py-3 pl-4", fullPreview && "max-sm:hidden")}
+          >
+            <h2 className={"text-gray-400 text-heading-s"}>MARKDOWN</h2>
+          </div>
+          <div
+            className={clsx(
+              "w-[1px] h-[full] border-r-gray-200 border-r-[1px]",
+              fullPreview && "max-sm:hidden",
+            )}
+          />
+        </>
+      )}
+      <div
+        className={clsx("flex-1 py-3 pl-4", !fullPreview && "max-sm:hidden")}
+      >
         <h2 className={"text-gray-400 text-heading-s"}>PREVIEW</h2>
       </div>
-      <PreviewIcon className={"absolute right-4 top-4"}></PreviewIcon>
+      {fullPreview ? (
+        <HidePreviewIcon
+          className={"absolute right-4 top-4"}
+          onClick={onToggleFullPreview}
+        />
+      ) : (
+        <ShowPreviewIcon
+          className={"absolute right-4 top-4"}
+          onClick={onToggleFullPreview}
+        />
+      )}
     </header>
   );
 }
