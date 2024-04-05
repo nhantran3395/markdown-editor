1aebf754570191122fa31e9172b5c27fd61f1fdd
nhantran3395 <thanhnhan33.95@gmail.com>
2024-04-05 16:08:31 +0700

    feat: implement markdown editing

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,16 +1,56 @@
 "use client";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { clsx } from "clsx";
 
 import { SubHeader } from "@/components/editor/sub-header";
 import { MarkdownPane } from "@/components/editor/markdown-pane";
 import { PreviewPane } from "@/components/editor/preview-pane";
 
+const DEFAULT_CONTENT = `# Welcome to Markdown
+
+Markdown is a lightweight markup language that you can use to add formatting elements to plaintext text documents.
+
+## How to use this?
+
+1. Write markdown in the markdown editor window
+2. See the rendered markdown in the preview window
+
+### Features
+
+- Create headings, paragraphs, links, blockquotes, inline-code, code blocks, and lists
+- Name and save the document to access again later
+- Choose between Light or Dark mode depending on your preference
+
+> This is an example of a blockquote. If you would like to learn more about markdown syntax, you can visit this [markdown cheatsheet](https://www.markdownguide.org/cheat-sheet/).
+
+#### Headings
+
+To create a heading, add the hash sign (#) before the heading. The number of number signs you use should correspond to the heading level. You'll see in this guide that we've used all six heading levels (not necessarily in the correct way you should use headings!) to illustrate how they should look.
+
+##### Lists
+
+You can see examples of ordered and unordered lists above.
+
+###### Code Blocks
+
+This markdown editor allows for inline-code snippets, like this: \`<p>I'm inline</p>\`. It also allows for larger code blocks like this:
+
+\`\`\`
+<main>
+  <h1>This is a larger code block</h1>
+</main>
+\`\`\``;
+
 export default function Home() {
   const [fullPreview, setFullPreview] = useState(false);
+  const [body, setBody] = useState(DEFAULT_CONTENT);
 
   function onToggleFullPreview() {
     setFullPreview((prev) => !prev);
+  }
+
+  function onChangeMarkdownContent(event: ChangeEvent<HTMLTextAreaElement>) {
+    setBody(event.target.value);
   }
 
   return (
@@ -26,7 +66,7 @@ export default function Home() {
               "flex-1 py-4 pl-4 border-r-gray-200 border-r-[1px] overflow-auto"
             }
           >
-            <MarkdownPane />
+            <MarkdownPane body={body} onBodyChange={onChangeMarkdownContent} />
           </div>
         )}
         <div
@@ -35,7 +75,7 @@ export default function Home() {
             !fullPreview && "max-sm:hidden",
           )}
         >
-          <PreviewPane />
+          <PreviewPane body={body} />
         </div>
       </section>
     </>
--- client/src/components/editor/markdown-pane.tsx
+++ client/src/components/editor/markdown-pane.tsx
@@ -1,48 +1,23 @@
-export function MarkdownPane() {
+import { ChangeEvent } from "react";
+
+type MarkdownPaneProps = {
+  body: string;
+  onBodyChange: (event: ChangeEvent<HTMLTextAreaElement>) => void;
+};
+
+export function MarkdownPane({
+  body,
+  onBodyChange,
+}: Readonly<MarkdownPaneProps>) {
   return (
-    <article className={"text-markup-body"}>
-      # Welcome to Markdown
-      <br />
-      Markdown is a lightweight markup language that you can use to add
-      formatting elements to plaintext text documents.
-      <br />
-      ## How to use this?
-      <br />
-      1. Write markdown in the markdown editor window
-      <br />
-      2. See the rendered markdown in the preview window
-      <br />
-      ### Features
-      <br />
-      - Create headings, paragraphs, links, blockquotes, inline-code, code
-      blocks, and lists
-      <br />
-      - Name and save the document to access again later
-      <br />
-      - Choose between Light or Dark mode depending on your preference
-      <br />
-      &gt; This is an example of a blockquote. If you would like to learn more
-      about markdown syntax, you can visit this [markdown
-      cheatsheet](https://www.markdownguide.org/cheat-sheet/).
-      <br />
-      #### Headings
-      <br />
-      To create a heading, add the hash sign (#) before the heading. The number
-      of number signs you use should correspond to the heading level. You'll see
-      in this guide that we've used all six heading levels (not necessarily in
-      the correct way you should use headings!) to illustrate how they should
-      look.
-      <br />
-      ##### Lists You can see examples of ordered and unordered lists above.
-      <br />
-      ###### Code Blocks
-      <br />
-      This markdown editor allows for inline-code snippets, like this: `
-      <p>I'm inline</p>`. It also allows for larger code blocks like this: ```
-      <main>
-        <h1>This is a larger code block</h1>
-      </main>
-      ```
+    <article className={"text-markup-body w-full h-full"}>
+      <textarea
+        value={body}
+        onChange={onBodyChange}
+        className={
+          "w-full h-full bg-background text-foreground resize-none outline-none"
+        }
+      />
     </article>
   );
 }
