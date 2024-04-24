7d77a347804851dbcd739708074baac56f04fe93
nhantran3395 <thanhnhan33.95@gmail.com>
2024-04-24 21:10:42 +0700

    test: add more assertions and update name to make it clearer

--- api/document-service/src/main/resources/data.sql
+++ api/document-service/src/main/resources/data.sql
@@ -2,3 +2,7 @@ INSERT INTO "DocumentContent" (document_content_id, content, created_date, modif
 VALUES (1, 'this is a default Markdown document', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 INSERT INTO "Document" (document_id, document_content_id, title, created_date, modified_date)
 VALUES (1, 1, 'default document', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO "DocumentContent" (document_content_id, content, created_date, modified_date)
+VALUES (2, 'hello, welcome to markdown editor', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO "Document" (document_id, document_content_id, title, created_date, modified_date)
+VALUES (2, 2, 'welcome.md', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
--- api/document-service/src/test/java/com/nhantran/markdowneditor/documentservice/adapter/in/web/GetDocumentPreviewsControllerTests.java
+++ api/document-service/src/test/java/com/nhantran/markdowneditor/documentservice/adapter/in/web/GetDocumentPreviewsControllerTests.java
@@ -11,6 +11,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.time.Instant;
 import java.util.List;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -28,24 +29,30 @@ class GetDocumentPreviewsControllerTests {
     @Test
     void returnsDocumentPreviews() throws Exception {
         Instant currentTime = Instant.now();
-        DocumentPreview documentPreview = new DocumentPreview(1L, "default document", currentTime, currentTime);
-        given(getDocumentPreviewsUseCase.getDocumentPreviews(null)).willReturn(List.of(documentPreview));
+        DocumentPreview documentPreview1 = new DocumentPreview(1L, "default document", currentTime, currentTime);
+        DocumentPreview documentPreview2 = new DocumentPreview(2L, "welcome", currentTime, currentTime);
+        given(getDocumentPreviewsUseCase.getDocumentPreviews(null)).willReturn(List.of(documentPreview1, documentPreview2));
 
         mockMvc.perform(
                         get("/documents/previews")
                                 .accept("application/json")
                 ).andExpect(status().isOk())
-                .andExpect(jsonPath("$..id").value(1))
-                .andExpect(jsonPath("$..title").value("default document"))
-                .andExpect(jsonPath("$..createdDate").value(currentTime.toString()))
-                .andExpect(jsonPath("$..modifiedDate").value(currentTime.toString()));
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].id").value(1))
+                .andExpect(jsonPath("$[0].title").value("default document"))
+                .andExpect(jsonPath("$[0].createdDate").value(currentTime.toString()))
+                .andExpect(jsonPath("$[0].modifiedDate").value(currentTime.toString()))
+                .andExpect(jsonPath("$[1].id").value(2))
+                .andExpect(jsonPath("$[1].title").value("welcome"))
+                .andExpect(jsonPath("$[1].createdDate").value(currentTime.toString()))
+                .andExpect(jsonPath("$[1].modifiedDate").value(currentTime.toString()));
 
 
         verify(getDocumentPreviewsUseCase).getDocumentPreviews(null);
     }
 
     @Test
-    void givenDocumentExists_whenFindByTitle_thenReturnsDocumentPreviews() throws Exception {
+    void givenDocumentWithMatchingTitleExists_whenFindByTitle_thenReturnsDocumentPreviews() throws Exception {
         Instant currentTime = Instant.now();
         String searchTerm = "default";
         DocumentPreview documentPreview = new DocumentPreview(1L, "default document", currentTime, currentTime);
@@ -56,6 +63,7 @@ class GetDocumentPreviewsControllerTests {
                         get("/documents/previews?title=" + searchTerm)
                                 .accept("application/json")
                 ).andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
                 .andExpect(jsonPath("$..id").value(1))
                 .andExpect(jsonPath("$..title").value("default document"))
                 .andExpect(jsonPath("$..createdDate").value(currentTime.toString()))
@@ -66,7 +74,7 @@ class GetDocumentPreviewsControllerTests {
     }
 
     @Test
-    void givenDocumentDoesNotExist_whenFindByTitle_thenReturnsEmptyList() throws Exception {
+    void givenDocumentWithMatchingTitleDoesNotExist_whenFindByTitle_thenReturnsEmptyList() throws Exception {
         Instant currentTime = Instant.now();
         String searchTerm = "markdown";
         DocumentPreview documentPreview = new DocumentPreview(1L, "default document", currentTime, currentTime);
