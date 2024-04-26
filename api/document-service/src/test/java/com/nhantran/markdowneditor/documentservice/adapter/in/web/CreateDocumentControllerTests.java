7ddc35e49ae3088bedd5e203c42bc8c2319e0b98
nhantran3395 <thanhnhan33.95@gmail.com>
2024-04-26 20:22:40 +0700

    test: add unit test for CreateDocumentController class

---
+++ api/document-service/src/test/java/com/nhantran/markdowneditor/documentservice/adapter/in/web/CreateDocumentControllerTests.java
@@ -0,0 +1,94 @@
+package com.nhantran.markdowneditor.documentservice.adapter.in.web;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nhantran.markdowneditor.documentservice.application.port.in.CreateDocumentCommand;
+import com.nhantran.markdowneditor.documentservice.application.port.in.CreateDocumentUseCase;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = CreateDocumentController.class)
+class CreateDocumentControllerTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    CreateDocumentUseCase createDocumentUseCase;
+
+    @Test
+    void givenMissingTitle_whenCreateDocument_thenReturnsBadRequest() throws Exception {
+        CreateDocumentCommand command = new CreateDocumentCommand(null, "sample document");
+
+        mockMvc.perform(
+                        post("/documents")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(asJsonString(command))
+                )
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void givenMissingContent_whenCreateDocument_thenReturnsCreatedStatusCode() throws Exception {
+        CreateDocumentCommand command = new CreateDocumentCommand("sample document", null);
+
+        given(createDocumentUseCase.createDocument(command)).willReturn(3L);
+
+        mockMvc.perform(
+                        post("/documents")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(asJsonString(command))
+                )
+                .andExpect(status().isCreated())
+                .andExpect(header().string("Location", "http://localhost/documents/3"));
+
+        verify(createDocumentUseCase).createDocument(command);
+    }
+
+    @Test
+    void givenMissingBothTitleAndContent_whenCreateDocument_thenReturnsBadRequest() throws Exception {
+        CreateDocumentCommand command = new CreateDocumentCommand(null, null);
+
+        mockMvc.perform(
+                        post("/documents")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(asJsonString(command))
+                )
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void givenTitleAndContent_whenCreateDocument_thenReturnsCreatedStatusCode() throws Exception {
+        CreateDocumentCommand command = new CreateDocumentCommand("sample document", "this is a sample markdown document");
+
+        given(createDocumentUseCase.createDocument(command)).willReturn(3L);
+
+        mockMvc.perform(
+                        post("/documents")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(asJsonString(command))
+                )
+                .andExpect(status().isCreated())
+                .andExpect(header().string("Location", "http://localhost/documents/3"));
+
+        verify(createDocumentUseCase).createDocument(command);
+    }
+
+    protected static String asJsonString(final Object obj) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            final String jsonContent = mapper.writeValueAsString(obj);
+            return jsonContent;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
