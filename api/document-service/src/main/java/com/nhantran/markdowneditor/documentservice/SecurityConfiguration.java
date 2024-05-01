56ded0304a0bea4d74798b195d5512173039d3cc
nhantran3395 <thanhnhan33.95@gmail.com>
2024-05-01 16:46:30 +0700

    WE-7 test: use persistence_test profile to resolve error with jacoco failure to generate the test report

--- api/document-service/src/main/java/com/nhantran/markdowneditor/documentservice/SecurityConfiguration.java
+++ api/document-service/src/main/java/com/nhantran/markdowneditor/documentservice/SecurityConfiguration.java
@@ -2,11 +2,13 @@ package com.nhantran.markdowneditor.documentservice;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@Profile("!persistence_test")
 public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
